f2d70152a2cd98d70424c0ad55d4fc1756df23f9
Tâm Lê <tam.le@dutycast.com>
2022-05-22 23:25:54 +0700

    lab02_1

---
+++ src/lab_02/lab02_1.java
@@ -0,0 +1,28 @@
+package lab_02;
+// Requirement:
+//Get input from user about height(m) and weight(kg) then calculate BMI
+    //Underweight = <18.5
+    //Normal weight = 18.5 – 24.9
+    //Overweight = 25–29.9
+    //Obesity = BMI of 30 or greater
+//BMI = weight / (height x 2)
+import java.util.Scanner;
+public class lab02_1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter your height (m)");
+        double height = input.nextDouble();
+        System.out.println("Please enter your weight (kg)");
+        double weight = input.nextDouble();
+        double BMI = weight / (height * 2);
+        if(BMI <= 18.5 ){
+            System.out.println("You're in the underweight range");
+        }else if(BMI > 18.5 && BMI <=24.9){
+            System.out.println("You're in the Normal weight range");
+        }else if(BMI <=25 && BMI <=29.9){
+            System.out.println("You're in the Overweight range");
+        }else{
+            System.out.println("You're in the Obesity range");
+        }
+    }
+}
