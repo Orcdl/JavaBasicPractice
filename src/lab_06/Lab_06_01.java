6bce757f570e695fee9e799a962e5201a4920b70
Tâm Lê <tam.le@dutycast.com>
2022-06-06 23:32:40 +0700

    complete lab06_01

---
+++ src/lab_06/Lab_06_01.java
@@ -0,0 +1,17 @@
+package lab_06;
+/*
+Requirement:
+Given input string: "2hrs and 5 minutes"
+Please calculate how many minutes in total
+ */
+public class Lab_06_01 {
+    public static void main(String[] args) {
+        String myInputTime = new String("2 hours and 5 mintues");
+        char hourvalue = myInputTime.charAt(0);
+        char mintuesvalue = myInputTime.charAt(12);
+        int myHour = Integer.parseInt(String.valueOf(hourvalue));
+        int myMintues = Integer.parseInt(String.valueOf(mintuesvalue));
+        int mintuesCovert = (myHour * 60);
+        System.out.println("Totlal mintues is " + (mintuesCovert + myMintues));
+    }
+}
