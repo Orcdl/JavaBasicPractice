07ea5b7754b724cf296ff8bfe415d5f44c9e0506
Orcdl <ltmt411@gmail.com>
2022-06-20 15:02:56 +0700

    update lab09

---
+++ src/lab_09/FullE2.java
@@ -0,0 +1,12 @@
+package lab_09;
+
+public class FullE2 extends Employee2{
+    FullE2(String inputTypeE){
+        this.typeE = inputTypeE;
+    }
+    @Override
+     int getSalary() {
+
+        return 50000;
+    }
+}
