0c971c09eb6eb81b691d62bf82c5c12c472b7475
Tam Le <tam.le@dutycast.com>
2024-02-27 00:00:52 +0700

    Question 3

---
+++ src/Question3/ArrayList.java
@@ -0,0 +1,23 @@
+package Question3;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ArrayList {
+
+    public static void main(String[] args) {
+        List<Integer> myIntArr = new java.util.ArrayList<>(Arrays.asList(1,2,3,4,5));
+
+        for (int index = 0; index < myIntArr.size(); index++) {
+            int targetNum = myIntArr.get(index);
+            boolean isOddNumber = targetNum % 2 !=0;
+            if(isOddNumber){
+                myIntArr.set(index, targetNum * 2);
+            }
+        }
+        for (int value : myIntArr) {
+            System.out.println(value);
+            
+        }
+    }
+}
